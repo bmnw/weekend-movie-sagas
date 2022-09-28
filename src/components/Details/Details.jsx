747e51eb6b6a3445d8e81cf97e847ab50100facc
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-09-28 14:11:58 -0500

    added To Movie List button to Details component to bring user back to home page with history.goBack onClick function

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 const Details = () => {
 
+    const history = useHistory();
     const genres = useSelector(store => store.movieGenres);
     const movieDetails = useSelector(store => store.movieDetails);
 
@@ -10,15 +12,20 @@ const Details = () => {
                 
                     {movieDetails.map(detail => {
                         return  <div key={detail.id}>
-                                    {detail.title}
-                                    <br />
-                                    {detail.description}       
-                                    <br />
-                                    <img src={detail.poster} />                
+                                    <div>
+                                        <img src={detail.poster} />     
+                                    </div>
+                                    <div>
+                                        <h2>{detail.title}</h2>  
+                                    </div>
+                                    <div>
+                                        <h4> {detail.description} </h4>    
+                                    </div>  
                                 </div>
                     })}
                 
-                
+                <button onClick={(event) => history.goBack('/')}>To Movie List</button>
+
             </div>
 } // end Details
 

8b2d935c7cf00fc60bdd6f542e7c56362e85203a
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-01 21:50:55 -0500

    added menu button, textfields and a genre dropdown menu button to AddMovie

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,5 +1,46 @@
+import {useState} from 'react';
+import MenuButton from '../MenuButton/MenuButton.jsx';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 const AddMovie = () => {
-    return  <> Add Movie
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    return  <> 
+                <MenuButton />
+                <Card>
+                    <TextField sx={{margin: 2}} label="Movie Title" variant="outlined" type='text'/>
+                    <TextField sx={{margin: 2}} label="Movie Description" variant="outlined" multiline type='text'/>
+                    <TextField sx={{margin: 2}} label="Movie Poster Link" variant="outlined" type='text'/>
+                    <TextField sx={{margin: 2}} label="Select a movie genre" variant="outlined" type='text'/>
+                    <div>
+                        <Button
+                            onClick={handleClick}
+                        >
+                            Movie Genres
+                        </Button>
+                        <Menu
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={(event) => history.push('/')}>Movie List</MenuItem>
+                        </Menu>
+              </div>
+                </Card>
             </>
 } // end AddMovie
 
